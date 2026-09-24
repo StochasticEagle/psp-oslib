8a574fe03fc016117e194fa6dfb3fbc116ed15fa
Aaron <179076459+StochasticEagle@users.noreply.github.com>
2026-09-24 02:06:33 -0600

    Replace MikMod with libxmp-lite

--- src/audio/mod.c
+++ src/audio/mod.c
@@ -1,196 +1,181 @@
 #include "oslib.h"
-#include <mikmod.h>
+#include <libxmp-lite/xmp.h>
 
-static int osl_modPlaying = 0;
-static int osl_modInitialized = 0;
+typedef struct {
+	xmp_context context;
+	int frequency;
+	int stereo;
+	int shift;
+	int playing;
+} OSL_MOD_DATA;
+
 static int osl_modFrequency = 44100;
 static int osl_modStereo = 1;
 static int osl_modShift = 0;
 
-static BOOL PSP_IsThere(void) {
-	return 1;
+static OSL_MOD_DATA *oslGetModData(OSL_SOUND *s) {
+	if (!s) {
+		return NULL;
+	}
+	return (OSL_MOD_DATA *)s->data;
 }
 
-static BOOL PSP_Init(void) {
-	if (VC_Init()) {
-		return 1;
+void oslAudioCallback_PlaySound_MOD(OSL_SOUND *s) {
+	OSL_MOD_DATA *mod = oslGetModData(s);
+	if (!mod) {
+		return;
 	}
 
-	return 0;
-}
-
-static void PSP_Exit(void) {
-	VC_Exit();
-}
-
-static BOOL PSP_Reset(void) {
-	VC_Exit();
-	return VC_Init();
-}
-
-static BOOL PSP_PlayStart(void) {
-	VC_PlayStart();
-	osl_modPlaying = 1;
-	return 0;
-}
-
-static void PSP_PlayStop(void) {
-	osl_modPlaying = 0;
-	VC_PlayStop();
-}
-
-static void PSP_Update(void) {
-}
-
-// Driver structure for MikMod to use PSP audio
-MIKMODAPI MDRIVER drv_psp = {
-	NULL,
-	"PSP Audio",
-	"PSP Output Driver v1.0 - by Jim Shaw",
-	0, 255,
-	"psp",
-	NULL,
-	NULL,
-	PSP_IsThere,
-	(void *)VC_SampleLoad,
-	VC_SampleUnload,
-	VC_SampleSpace,
-	VC_SampleLength,
-	PSP_Init,
-	PSP_Exit,
-	PSP_Reset,
-	VC_SetNumVoices,
-	PSP_PlayStart,
-	PSP_PlayStop,
-	PSP_Update,
-	NULL,
-	VC_VoiceSetVolume,
-	VC_VoiceGetVolume,
-	VC_VoiceSetFrequency,
-	VC_VoiceGetFrequency,
-	VC_VoiceSetPanning,
-	VC_VoiceGetPanning,
-	VC_VoicePlay,
-	VC_VoiceStop,
-	VC_VoiceStopped,
-	VC_VoiceGetPosition,
-	VC_VoiceRealVolume
-};
-
-/*
-    Callbacks for MOD sound playback
- */
-void oslAudioCallback_PlaySound_MOD(OSL_SOUND *s) {
-	// Stereo only, mono doesn't work yet
-	md_mode = DMODE_16BITS | DMODE_STEREO | DMODE_SOFT_MUSIC;
-	s->mono = 0;
-
-	Player_Stop();
-	Player_Start((MODULE *)s->data);
-	Player_SetPosition(0);
+	xmp_end_player(mod->context);
+	mod->playing = (xmp_start_player(mod->context, mod->frequency, mod->stereo ? 0 : XMP_FORMAT_MONO) == 0);
+	if (mod->playing) {
+		xmp_play_buffer(mod->context, NULL, 0, 0);
+	}
 }
 
 void oslAudioCallback_StopSound_MOD(OSL_SOUND *s) {
-	Player_Stop();
+	OSL_MOD_DATA *mod = oslGetModData(s);
+	if (!mod) {
+		return;
+	}
+
+	xmp_end_player(mod->context);
+	mod->playing = 0;
 }
 
 int oslAudioCallback_AudioCallback_MOD(unsigned int i, void *buf, unsigned int length) {
-	// Set up playback
-	md_mixfreq = osl_modFrequency;
+	OSL_SOUND *s = osl_audioVoices[i].sound;
+	OSL_MOD_DATA *mod = oslGetModData(s);
+	unsigned int scale;
+	unsigned int sourceFrames;
+	unsigned int channels;
+	unsigned int sourceBytes;
+	short *samples;
+	int result;
 
-	if (osl_modPlaying) {
-		VC_WriteBytes(buf, length << 2);
-	} else {
-		memset(buf, 0, length << 2);
+	if (!mod || !mod->playing || length == 0) {
+		if (buf && length > 0) {
+			unsigned int bytesPerFrame = (s && s->mono == 0x10) ? 2 : 4;
+			memset(buf, 0, length * bytesPerFrame);
+		}
+		return 0;
 	}
 
-	// End of the song
-	if (!Player_Active()) {
-		return 0; // Stop playing
+	channels = mod->stereo ? 2 : 1;
+	scale = 1U << mod->shift;
+	sourceFrames = (length + scale - 1) / scale;
+	sourceBytes = sourceFrames * channels * sizeof(short);
+
+	result = xmp_play_buffer(mod->context, buf, (int)sourceBytes, s->endCallback == oslSoundLoopFunc ? 0 : 1);
+	if (result < 0) {
+		memset(buf, 0, length * channels * sizeof(short));
+		mod->playing = 0;
+		return 0;
 	}
 
-	return 1; // Continue playing
+	if (mod->shift > 0) {
+		samples = (short *)buf;
+		for (int outputFrame = (int)length - 1; outputFrame >= 0; outputFrame--) {
+			unsigned int sourceFrame = (unsigned int)outputFrame >> mod->shift;
+			for (unsigned int channel = 0; channel < channels; channel++) {
+				samples[(unsigned int)outputFrame * channels + channel] = samples[sourceFrame * channels + channel];
+			}
+		}
+	}
+
+	return 1;
 }
 
 VIRTUAL_FILE **oslAudioCallback_ReactiveSound_MOD(OSL_SOUND *s, VIRTUAL_FILE *f) {
-	// Reactivate a MOD sound (not implemented)
+	(void)s;
+	(void)f;
 	return NULL;
 }
 
 VIRTUAL_FILE *oslAudioCallback_StandBy_MOD(OSL_SOUND *s) {
-	// Put a MOD sound on standby (not implemented)
+	(void)s;
 	return NULL;
 }
 
 void oslAudioCallback_DeleteSound_MOD(OSL_SOUND *s) {
-	MikMod_free((MODULE *)s->data);
+	OSL_MOD_DATA *mod = oslGetModData(s);
+	if (!mod) {
+		return;
+	}
+
+	xmp_end_player(mod->context);
+	xmp_release_module(mod->context);
+	xmp_free_context(mod->context);
+	free(mod);
+	s->data = NULL;
 }
 
-/*
-    Error handler for MikMod errors
- */
-void oslHandleModError(void) {
-	oslDebug("_mm_critical %i\nmm_errno %i\n%s", MikMod_critical, MikMod_errno, MikMod_strerror(MikMod_errno));
-}
-
-/*
-    Set the sample rate and stereo configuration for MOD playback
-    Example:
-        oslSetModSampleRate(11025, 2); // Low CPU, bad sound
-        oslSetModSampleRate(22050, 1); // Low CPU, medium sound
-        oslSetModSampleRate(44100, 0); // Normal CPU, good sound
- */
 void oslSetModSampleRate(int freq, int stereo, int shift) {
+	if (freq < XMP_MIN_SRATE) {
+		freq = XMP_MIN_SRATE;
+	} else if (freq > XMP_MAX_SRATE) {
+		freq = XMP_MAX_SRATE;
+	}
+	if (shift < 0) {
+		shift = 0;
+	} else if (shift > 30) {
+		shift = 30;
+	}
+
 	osl_modFrequency = freq;
-	osl_modStereo = stereo;
+	osl_modStereo = stereo ? 1 : 0;
 	osl_modShift = shift;
 }
 
 OSL_SOUND *oslLoadSoundFileMOD(const char *filename, int stream) {
-	OSL_SOUND *s;
-	MODULE *mf;
+	OSL_SOUND *s = NULL;
+	OSL_MOD_DATA *mod = NULL;
 
-	if (!osl_modInitialized) {
-		MikMod_RegisterAllLoaders();
-		MikMod_RegisterDriver(&drv_psp);
-
-		md_mode = DMODE_16BITS | DMODE_STEREO | DMODE_SOFT_MUSIC;
-		MikMod_Init("");
-
-		osl_modInitialized = 1;
-	}
+	(void)stream;
 
 	s = (OSL_SOUND *)malloc(sizeof(OSL_SOUND));
-	if (s) {
-		memset(s, 0, sizeof(OSL_SOUND));
-		mf = Player_Load((char *)filename, 128, 0);
-		if (mf) {
-			// Populate the OSL_SOUND structure with the loaded MOD file
-			s->data = (void *)mf;
-			s->endCallback = NULL;  // No end callback for MOD files
-			s->volumeLeft = s->volumeRight = OSL_VOLUME_MAX;
-			s->format = 0;          // No special format
-			s->mono = 0;            // Always stereo output
-			s->divider = OSL_FMT_44K; // Default to 44K format
-			s->isStreamed = 0;      // MOD files are never streamed
-			s->numSamples = 0;      // Default value
-
-			// Assign callback functions
-			s->audioCallback = oslAudioCallback_AudioCallback_MOD;
-			s->playSound = oslAudioCallback_PlaySound_MOD;
-			s->stopSound = oslAudioCallback_StopSound_MOD;
-			s->standBySound = oslAudioCallback_StandBy_MOD;
-			s->reactiveSound = oslAudioCallback_ReactiveSound_MOD;
-			s->deleteSound = oslAudioCallback_DeleteSound_MOD;
-		} else {
-			free(s);
-			s = NULL;
-		}
+	mod = (OSL_MOD_DATA *)malloc(sizeof(OSL_MOD_DATA));
+	if (!s || !mod) {
+		goto error;
 	}
 
-	if (!s) {
-		oslHandleLoadNoFailError(filename);
+	memset(s, 0, sizeof(OSL_SOUND));
+	memset(mod, 0, sizeof(OSL_MOD_DATA));
+
+	mod->context = xmp_create_context();
+	if (!mod->context || xmp_load_module(mod->context, filename) < 0) {
+		goto error;
 	}
 
+	mod->frequency = osl_modFrequency;
+	mod->stereo = osl_modStereo;
+	mod->shift = osl_modShift;
+
+	s->data = mod;
+	s->endCallback = NULL;
+	s->volumeLeft = s->volumeRight = OSL_VOLUME_MAX;
+	s->format = 0;
+	s->mono = mod->stereo ? 0 : 0x10;
+	s->divider = OSL_FMT_44K;
+	s->isStreamed = 0;
+	s->numSamples = 0;
+	s->audioCallback = oslAudioCallback_AudioCallback_MOD;
+	s->playSound = oslAudioCallback_PlaySound_MOD;
+	s->stopSound = oslAudioCallback_StopSound_MOD;
+	s->standBySound = oslAudioCallback_StandBy_MOD;
+	s->reactiveSound = oslAudioCallback_ReactiveSound_MOD;
+	s->deleteSound = oslAudioCallback_DeleteSound_MOD;
 	return s;
+
+error:
+	if (mod) {
+		if (mod->context) {
+			xmp_release_module(mod->context);
+			xmp_free_context(mod->context);
+		}
+		free(mod);
+	}
+	free(s);
+	oslHandleLoadNoFailError(filename);
+	return NULL;
 }
